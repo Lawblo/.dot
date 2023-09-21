c728488be962ee6de86436f93991cea791299c83
thorkil <schjodt.thorkil@gmail.com>
2023-09-21 17:20:52 +0200

    nvim nightly script done, started on zshrc

--- setup.sh
+++ setup.sh
@@ -1,4 +1,24 @@
-mkdir
-
-ln -s ~/linux_setup/nvim 
 apt install zsh -y
+
+
+if ! [ -d ~/.config ];
+then 
+	mkdir ~/.config
+fi
+
+if ! [ -d ~/.oh-my-zsh ];
+then 
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+createSymlink() {
+	if ! [ -L $2 ];
+	then
+		rm $2 -rf
+		ln -s $1 $2
+	fi
+}
+
+createSymlink ~/linux_setup/nvim ~/.config/nvim
+createSymlink ~/linux_setup/tmux ~/.config/tmux
+createSymlink ~/linux_setup/zshrc ~/.zshrc
